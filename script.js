247581920b0fcf099db2da60b4957e5afdbbae29
Magenta1028 <74944468+magenta1028@users.noreply.github.com>
2021-01-10 12:04:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ client.on("message", (chat) => {
 async function login(email, password) {
     try {
         const loginRes = await client.login(email, password);
-        console.log("[+] - 로그인에 성공했습니다.");
+        console.log("[Log - login] - 로그인에 성공했습니다.");
     } catch (error) {
         if (error.status === node_kakao.AuthStatusCode.DEVICE_NOT_REGISTERED) {
             const requestPasscodeRes = await client.Auth.requestPasscode(email, password, true);
             if (requestPasscodeRes.status === node_kakao.StatusCode.SUCCESS) {
-                console.error("[!] - 인증이 필요합니다. 인증번호를 요청했으니 입력해주세요.");
+                console.error("[Error - login] - 인증이 필요합니다. 인증번호를 요청했으니 입력해주세요.");
                 const readline = require("readline");
                 const rl = readline.createInterface({
                     input: process.stdin,
@@ -32,18 +32,19 @@ async function login(email, password) {
                 rl.question("인증코드: ", async (passcode) => {
                     const registerDeviceRes = await client.Auth.registerDevice(passcode, email, password, true);
                     if (registerDeviceRes.status === node_kakao.StatusCode.SUCCESS) {
-                        console.log("[+] 인증이 완료되었습니다. 스크립트를 다시 실행해주세요.");
+                        console.log("[Log - login] 인증이 완료되었습니다. 스크립트를 다시 실행해주세요.");
                         process.exit();
                     } else {
-                        console.error(`[!] - 인증을 하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(registerDeviceRes, null, 2)}`);
+                        console.error(`[Error - login] - 인증을 하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(registerDeviceRes, null, 2)}`);
                         process.exit();
                     }
                 });
             } else {
-                process.exit(`[!] - 인증이 필요하여 인증번호를 요청하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(requestPasscodeRes, null, 2)}`);
+                console.error(`[Error - login] - 인증이 필요하여 인증번호를 요청하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(requestPasscodeRes, null, 2)}`);
+                process.exit();
             }
         } else {
-            console.error(`[!] - 로그인을 하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(error, null, 2)}`);
+            console.error(`[Error - login] - 로그인을 하는 과정에서 오류가 발생했습니다. 오류 JSON: ${JSON.stringify(error, null, 2)}`);
             process.exit();
         }
     }
